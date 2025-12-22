025024c846beb8c0b7c618e04b0a7a6e9fbeeec5
sreeja-dende <154778149+sreeja-dende@users.noreply.github.com>
2025-12-22 17:55:04 +0530

    Add Valid_Anagram class to check for anagrams

---
+++ Arrays and Hashing/Valid_Anagram.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+    if(s.length()==t.length())
+    { char a[]=s.toCharArray();
+      char b[]=t.toCharArray();
+          Arrays.sort(a);
+          Arrays.sort(b);
+          if(Arrays.equals(a,b))
+          {return true;
+          }
+    } return false; 
+    }
+}
