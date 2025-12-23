66bdc0ea77739e1545afbb1c6848d4649010feb4
sreeja-dende <154778149+sreeja-dende@users.noreply.github.com>
2025-12-23 17:40:16 +0000

    Implement topKFrequent method to find top K elements

---
+++ Arrays and Hashing/topk _frequent _elements.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+         Map<Integer, Integer> freqMap = new HashMap<>();
+        for (int num : nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+        PriorityQueue<Integer> maxHeap =
+            new PriorityQueue<>((a, b) -> freqMap.get(b) - freqMap.get(a));
+
+        for (int num : freqMap.keySet()) {
+            maxHeap.offer(num);
+        }
+
+    
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++) {
+            result[i] = maxHeap.poll();
+        }
+
+        return result;
+    }
+}
