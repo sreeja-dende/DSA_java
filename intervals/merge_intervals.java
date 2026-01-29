20a3bf8a5654d9deb010a686e414f19dfa717957
sreeja-dende <154778149+sreeja-dende@users.noreply.github.com>
2026-01-29 19:33:34 +0530

    Implement merge intervals algorithm

---
+++ intervals/merge_intervals.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+         if (intervals.length <= 1) return intervals;
+        List<int[]> merged =new ArrayList<>();
+       Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+       int[] current = intervals[0];
+        for (int i = 1; i < intervals.length; i++) {
+            
+            if (current[1] >= intervals[i][0]) {
+                current[1] = Math.max(current[1], intervals[i][1]);
+            } else {
+                
+                merged.add(current);
+                current = intervals[i];
+            }
+        }
+  merged.add(current);
+
+        return merged.toArray(new int[merged.size()][]);
+    }
+}
+
