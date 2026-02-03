ead4671c789b5733fe677b336b26b9c4d3cb4d19
sreeja-dende <154778149+sreeja-dende@users.noreply.github.com>
2026-02-03 11:51:19 +0000

    Implement max subarray sum using Kadane's algorithm

---
+++ greedy/max_sub_array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+         int max_sum=Integer.MIN_VALUE;
+         int sum=0;
+        for(int i=0;i<nums.length;i++)
+        {  
+           sum=sum+nums[i];
+           max_sum=Math.max(max_sum,sum);
+           if(sum<0)
+           {
+            sum=0;
+           }
+        }
+        return max_sum;
+    }
+}
